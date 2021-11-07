5ba31cefd6a024e71901f6005794ad94a8e58805
Bogdan Dit <bogdandit@boisestate.edu>
2021-11-07 14:04:58 -0700

    added starter code for GeneBankSearchDatabase

---
+++ src/main/java/cs321/search/GeneBankSearchDatabase.java
@@ -0,0 +1,16 @@
+package cs321.search;
+
+import cs321.btree.BTree;
+import cs321.common.ParseArgumentException;
+import cs321.common.ParseArgumentUtils;
+
+public class GeneBankSearchDatabase
+{
+
+    public static void main(String[] args) throws Exception
+    {
+        System.out.println("Hello world from cs321.search.GeneBankSearchDatabase.main");
+
+    }
+
+}
