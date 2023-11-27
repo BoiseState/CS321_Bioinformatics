2039f4209cfa10f26036483aba0578d6f76789ba
Amit Jain <ajain@boisestate.edu>
2023-11-27 11:51:18 -0700

    Fix parse4CoorectArgumentsTest to use named command line arguments

--- src/test/java/cs321/create/GeneBankCreateBTreeTest.java
+++ src/test/java/cs321/create/GeneBankCreateBTreeTest.java
@@ -16,10 +16,10 @@ public class GeneBankCreateBTreeTest
     public void parse4CorrectArgumentsTest() throws ParseArgumentException
     {
         args = new String[4];
-        args[0] = "0";
-        args[1] = "20";
-        args[2] = "fileNameGbk.gbk";
-        args[3] = "13";
+        args[0] = "--cache=0";
+        args[1] = "--degree=20";
+        args[2] = "--gbkfile=fileNameGbk.gbk";
+        args[3] = "--length=13";
 
         expectedConfiguration = new GeneBankCreateBTreeArguments(false, 20, "fileNameGbk.gbk", 13, 0, 0);
         actualConfiguration = GeneBankCreateBTree.parseArguments(args);
